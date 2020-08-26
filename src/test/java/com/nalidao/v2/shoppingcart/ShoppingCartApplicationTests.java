10fdf9a8a09024504e87f4329050011c2d175a5d
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2020-08-26 10:04:12 -0300

    Comiit inicial

---
+++ src/test/java/com/nalidao/v2/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalidao.v2.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
